96cdece6c5d06cf7a59992aead360c3680668fc7
Useit14 <useit14@inbox.ru>
2022-08-01 22:50:35 +0300

    Lesson03

--- script.js
+++ script.js
@@ -12,3 +12,60 @@ let numToPower3 = mult ** 3;
 
 let numToPower3Str = numToPower3.toString();
 console.log(numToPower3Str.substring(0, 2));
+
+//lesson03
+
+let lang = prompt("Введите язык", "ru");
+
+const daysRu = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
+];
+const daysEng = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday ",
+  "Friday ",
+  "Saturday",
+  "Sunday ",
+];
+
+if (lang == "ru") {
+  daysRu.forEach((element) => {
+    console.log(element);
+  });
+} else if (lang == "en") {
+  daysEng.forEach((element) => {
+    console.log(element);
+  });
+}
+
+switch (lang) {
+  case "ru":
+    daysRu.forEach((element) => {
+      console.log(element);
+    });
+    break;
+  case "en":
+    daysEng.forEach((element) => {
+      console.log(element);
+    });
+    break;
+}
+
+const days = [daysRu, daysEng];
+lang == "ru" ? console.log(days[0].join(",")) : console.log(days[1].join(","));
+
+const namePerson = prompt("Введите имя", "Артем");
+
+namePerson == "Артем"
+  ? console.log("Директор")
+  : namePerson == "Александр"
+  ? console.log("Преподователь")
+  : console.log("Студент");
