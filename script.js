1dbb6ae2b8f0ca8f49455ae8287fd3ae3e83788d
Useit14 <useit14@inbox.ru>
2022-08-02 10:15:20 +0300

    Changes something

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let num = 266219;
-let numStr = num.toString();
-
-let mult = 1;
-for (let index = 0; index < numStr.length; index++) {
-  const element = +numStr[index];
-  mult *= element;
-}
-console.log(mult);
-
-let numToPower3 = mult ** 3;
-
-let numToPower3Str = numToPower3.toString();
-console.log(numToPower3Str.substring(0, 2));
-
-//lesson03
-
 let lang = prompt("Введите язык", "ru");
 
 const daysRu = [
@@ -64,7 +47,7 @@ lang == "ru" ? console.log(days[0].join(",")) : console.log(days[1].join(","));
 
 const namePerson = prompt("Введите имя", "Артем");
 
-namePerson == "Артем"
+namePerson === "Артем"
   ? console.log("Директор")
   : namePerson == "Александр"
   ? console.log("Преподователь")
