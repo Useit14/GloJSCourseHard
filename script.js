f7207fd13ddccb11e0fed09c6f4f455a84646b45
Useit14 <useit14@inbox.ru>
2022-08-10 23:07:46 +0300

    mission is complete

--- script.js
+++ script.js
@@ -8,17 +8,67 @@ const week = [
   "Воскресенье",
 ];
 
+const month = [
+  "Января",
+  "Февраля",
+  "Марта",
+  "Апреля",
+  "Мая",
+  "Июня",
+  "Июля",
+  "Августа",
+  "Сентября",
+  "Октября",
+  "Ноября",
+  "Декабря",
+];
+
 const root = document.getElementById("main");
 const date = new Date();
-for (const day of week) {
-  const element = document.createElement("div");
-  element.textContent = day;
-  if (day == "Суббота" || day == "Воскресенье") {
-    element.style = "font-style:italic";
-  } else if (day == week[date.getDay() - 1]) {
-    element.style = "font-weight:bold";
-  }
-  root.append(element);
-}
+const element1 = document.createElement("div");
+const element2 = document.createElement("div");
 
-console.log(date.getDay());
+const getHourWord = () => {
+  const hours = new Date().getHours();
+  if (hours == 1) {
+    return "час";
+  } else if ([2, 3, 4].includes(parseInt(hours.toString()[1]))) {
+    return "часа";
+  } else {
+    return "часов";
+  }
+};
+
+const addZero = (date) => {
+  let arr = date.toString().split("-");
+  let count = 0;
+  arr.forEach((element) => {
+    let innerArr = element.split(".");
+    let count1 = 0;
+    innerArr.forEach((element) => {
+      if (element.length == 1) {
+        innerArr[count1] = "0" + element;
+      }
+      count1++;
+    });
+    arr[count] = innerArr.join(".");
+    count++;
+  });
+  return arr.join("-");
+};
+
+root.append(element1);
+root.append(element2);
+
+element1.textContent = `Сегодня ${week[date.getDay() - 1]}, ${date.getDate()} ${
+  month[date.getMonth()]
+} ${date.getFullYear()} года, ${date.getHours()} ${getHourWord()} ${date.getMinutes()} минут ${date.getSeconds()} секунды `;
+
+setInterval(() => {
+  const date1 = new Date();
+  element2.textContent = addZero(
+    `${date1.getDate()}.${
+      date1.getMonth() + 1
+    }.${date1.getFullYear()} - ${date1.getHours()}.${date1.getMinutes()}.${date1.getSeconds()}`
+  );
+}, 100);
